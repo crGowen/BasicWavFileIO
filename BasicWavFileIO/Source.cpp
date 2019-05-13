c6f47c89fbf6ba1031e947e8aa19175b138e6f86
Ryan Gowen <39204053+crGowen@users.noreply.github.com>
2019-05-13 16:08:24 +0100

    commenting code

--- BasicWavFileIO/Source.cpp
+++ BasicWavFileIO/Source.cpp
@@ -7,9 +7,9 @@ namespace wavio
 	{
 	public:
 		char groupID[4]; // always "RIFF"
-		unsigned int length;
+		unsigned int fileBytes; // size of file in minus 8 bytes
 		char typeRIFF[4]; // always "WAVE"
-	};
+	}; //sizeof should return 12 bytes
 
 	class FormatChunk
 	{
@@ -17,21 +17,22 @@ namespace wavio
 		char chunkID[4]; // ALWAYS "fmt " for FormatChunk
 		unsigned int chunkSize;
 		unsigned short formatType;
-		unsigned short nChannels;
+		unsigned short nChannels; 
 		unsigned int samplesFreq;
 		unsigned int estBytesFreq;
 		unsigned short sampleFrameSize;
-		unsigned int bitsPerSample;
-	};
+		unsigned int bitsPerSample; // padded by 2 bytes to the 24th byte in Format chunk (total size of chunk is 28bytes).
+	}; // size of should return 28bytes
 
 	class DataChunk
 	{
 	public:
 		char chunkID[4]; // ALWAYS "data" for FormatChunk
 		unsigned int chunkSize;
-		unsigned char* uDataArray; //unsigned data / char 8bit
-		short* sDataArray; //signed data / short 16bit
-	};
+		unsigned char* byteDataArray; //unsigned data / char 8bit (perhaps with 16bit data, skip all even bytes to get the 2 least signifs from 16 bit data chunks)
+		// padding isn't an issue here, because when writing the data chunk we'll write the first two vars first, then since the byteDataArray is dynamic memory, write it 1 byte at a time
+		// so we don't write DataChunk to a file, we write chunkID, chunkSize, then byteDataArray (however big it will be)
+	}; // sizeof should return 16 bytes (8 of which are the just the 64b pointer).
 
 	// KEEP IT SIMPLE, it needs to be pure P.O.D.
 	class WavFileData
